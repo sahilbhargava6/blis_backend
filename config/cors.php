c5b689a22118e0611eab9fa93393205f63ee1d9c
sahilbhargava6 <sahilbhargava6@gmail.com>
2026-07-02 23:36:16 +0530

    Allow localhost ports in CORS for local development

--- config/cors.php
+++ config/cors.php
@@ -18,9 +18,13 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
-
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'http://localhost:3000'), 
+        'http://localhost:3000', 
+        'http://localhost:3001',
+        'http://localhost:3002',
+        'http://localhost:3004'
+    ],
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
